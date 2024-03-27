c3b20d1a58e97a492fa801e98ee534d8af210b1b
Ahmed Hassan <ahmedmohamedhassan1to10@gmail.com>
2024-03-27 05:35:36 +0200

    handle k means test

--- built_in_k_means.py
+++ built_in_k_means.py
@@ -1,11 +1,7 @@
-from extractData import *
 from evaluation import Evaluation
-from k_means import KMeans
-
-
+from extractData import *
 import numpy as np
 from sklearn.cluster import KMeans
-from sklearn.metrics import f1_score
 
 def compute_testing_clusters(testing_data, centroids):
     clusters = np.argmin(np.linalg.norm(testing_data[:, None] - centroids, axis=2), axis=1)
@@ -15,8 +11,9 @@ def compute_testing_clusters(testing_data, centroids):
 def apply_kmeans(training_data, test_data, test_labels, k_values):
     for k in k_values:
         print("K:", k)
-        kmeans = KMeans(k=k)
-        _, centroids = kmeans.fit_predict(training_data)
+        kmeans = KMeans(n_clusters=k)
+        kmeans.fit(training_data)
+        centroids = kmeans.cluster_centers_
         testing_clusters = compute_testing_clusters(test_data, centroids)
         evaluation = Evaluation(testing_clusters, test_labels)
         evaluation.evaluate()
@@ -24,23 +21,11 @@ def apply_kmeans(training_data, test_data, test_labels, k_values):
 
 
 if __name__ == "__main__":
-    training_data, test_data, tL, test_labels = get_data_for_solution_one()
-    # Assuming you have loaded your data into 'data' and 'data_labels'
-
-    # Apply KMeans
-    kmeans = KMeans(n_clusters=19)
-    kmeans.fit(training_data)
-    predicted_labels = kmeans.labels_
-
-    # Evaluate with F1 Score
-    f1 = f1_score(tL, predicted_labels, average='none')
-    print("F1 Score:", f1)
-    print(f1)
+    print("Built in K-Means Results for Solution One and Two")
+    training_data, test_data, _, test_labels = get_data_for_solution_one()
     k_values = [8, 13, 19, 28, 38]
     print("Solution One On Mean Data")
     apply_kmeans(training_data, test_data, test_labels, k_values)
     training_data, test_data, _, test_labels = get_data_for_solution_two()
     print("Solution Two On Flattened Data And PCA")
     apply_kmeans(training_data, test_data, test_labels, k_values)
-
-
